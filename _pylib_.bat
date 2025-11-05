da33d2553a8c0e0a1d75c0778e5c9b83a9cb218f
dingxiuan <dingxiuan@163.com>
2025-11-05 09:37:13 +0800

    修改依赖的下载地址

--- _pylib_.bat
+++ _pylib_.bat
@@ -1,4 +1,5 @@
 
 
 pip uninstall pylib -y
-pip install pylib --index-url=http://admin:admin123@127.0.0.1:9001/repository/pypi-group/simple --trusted-host=127.0.0.1
+@REM pip install pylib --index-url=http://admin:admin123@127.0.0.1:9001/repository/pypi-group/simple --trusted-host=127.0.0.1
+pip install pylib --index-url=http://admin:admin123@192.168.1.200:9001/repository/pypi-group/simple --trusted-host=192.168.1.200
