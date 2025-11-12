26bed235b0ec8b0b2c7aa364c20ce654ddcfd2cf
dingxiuan <dafeisuowen01@163.com>
2025-11-12 21:48:27 +0800

    优化打包

--- _pkg_.bat
+++ _pkg_.bat
@@ -2,9 +2,15 @@
 
 pip install pyinstaller
 
-pyinstaller --onefile app.py
+@echo off
+set appName=pyside6-app
 
+:: 直接使用变量
+echo app name ==>: %appName%
+
+@REM pyinstaller --onefile app.py
 @REM pyinstaller --onefile -F -w app.py
+pyinstaller --noconfirm --onedir -w --name %appName% app.py
 
-copy .\conf.json .\dist\
-move .\dist\app.exe .\dist\pyside6-app.exe
+copy .\conf.json .\dist\%appName%\
+@REM move .\dist\app.exe .\dist\%appName%.exe
--- _pkg_single_.bat
+++ _pkg_single_.bat
@@ -2,9 +2,15 @@
 
 pip install pyinstaller
 
-pyinstaller --onefile app_single.py
+@echo off
+set appName=pyside6-app
 
+:: 直接使用变量
+echo app name ==>: %appName%
+
+@REM pyinstaller --onefile app_single.py
 @REM pyinstaller --onefile -F -w app_single.py
+pyinstaller --noconfirm --onedir -w --name %appName% app_single.py
 
-copy .\conf.json .\dist\
-move .\dist\app_single.exe .\dist\pyside6-app.exe
+copy .\conf.json .\dist\%appName%\
+@REM move .\dist\app_single.exe .\dist\%appName%.exe
